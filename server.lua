a51b0127329e136d7abda7242a544fb23c521012
EdyzPW <94766321+EdyzPW@users.noreply.github.com>
2024-11-10 11:43:28 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,7 +1,8 @@
-AddEventHandler('ox_inventory:openedInventory', function(source)
-    TriggerClientEvent('policeradar:toggleRadar', source)
-end)
-
-AddEventHandler('ox_inventory:closedInventory', function(source)
-    TriggerClientEvent('policeradar:toggleRadar', source)
-end)
+if GetResourceState('ox_inventory') == 'started' then
+    AddEventHandler('ox_inventory:openedInventory', function(source)
+        TriggerClientEvent('policeradar:toggleRadar', source)
+    end)
+    AddEventHandler('ox_inventory:closedInventory', function(source)
+        TriggerClientEvent('policeradar:toggleRadar', source)
+    end)
+end
