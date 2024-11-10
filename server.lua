2531795250d3b422f72ee6b19d902d9801fbefd3
EdyzPW <94766321+EdyzPW@users.noreply.github.com>
2024-11-10 11:27:59 +0200

    Create server.lua

---
+++ server.lua
@@ -0,0 +1,7 @@
+AddEventHandler('ox_inventory:openedInventory', function(source)
+    TriggerClientEvent('policeradar:toggleRadar', source)
+end)
+
+AddEventHandler('ox_inventory:closedInventory', function(source)
+    TriggerClientEvent('policeradar:toggleRadar', source)
+end)
